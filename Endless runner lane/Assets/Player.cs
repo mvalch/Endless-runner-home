e722e4698a5b3668150b53e3bceece7c612b40ca
mvalch <mvalchanova2022@elam.co.uk>
2022-10-05 23:33:47 +0100

    Made player move up or down
    added max + min height
    no going off screen
    smoother transition

---
+++ Endless runner lane/Assets/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+   private Vector2 targetPos;
+   public float Yincrement;
+
+   public float speed;
+   public float maxHeight;
+   public float minHeight;
+
+   private void Update()
+   {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight) {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight) {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            
+        }
+   }
+}
