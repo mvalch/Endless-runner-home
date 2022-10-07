9e9fd7e5d266f0653a52d291f19f99ecce3535b2
mvalch <mvalchanova2022@elam.co.uk>
2022-10-07 12:44:08 +0100

    added more to spawner script - decrease time

--- Endless runner lane/Assets/Spawner.cs
+++ Endless runner lane/Assets/Spawner.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject Obstacle;
+    public GameObject obstacle;
 
     public float timeBtwSpawn;
     public float startTimeBtwSpawn;
+    public float decreaseTime;
+    public float minTime = 0.65f;
 
     private void Update()
     {
-        if (timeBtwSpawn <= 0) {
-            Instantiate(Obstacle, transform.position, Quaternion.identity);
+        if (timeBtwSpawn <= 0) 
+        {
+            Instantiate(obstacle, transform.position, Quaternion.identity);
             timeBtwSpawn = startTimeBtwSpawn;
+            if (startTimeBtwSpawn > minTime)
+            {
+                startTimeBtwSpawn -= decreaseTime;
+            }
+            
         }
         else {
             timeBtwSpawn -= Time.deltaTime;
